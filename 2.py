fd5bb6b135c39ab81ecebfc077f3437d8c2e0fbd
jingtianzhao <57737141+jingtianzhao@users.noreply.github.com>
2020-03-17 21:16:27 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,17 @@
 import tensorflow as tf
-import numpy as np
-priint('hello world')
+import numpy as n
+class dog:
+    def __init__(self,weight,hight,blood,power):
+        self.hight =hight
+        self.weight=weight
+        self.power=power
+        self.blood=blood
+    def attach(self,dog2):
+        dog2.blood =dog2.blood -self.power
+    def bark(self):
+        print(f'me blood is {self.blood} my power is {self.power}')
+d1 =dog(1,2,10,2)
+d2 =dog(1,3,15,3)
+d1.bark()
+d1.attach(d2)
+d2.bark()
